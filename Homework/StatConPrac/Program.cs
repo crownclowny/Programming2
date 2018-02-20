e217a3016e2f181b7bf618a2b45d7a9b8d9c71ec
crownclowny <35781653+crownclowny@users.noreply.github.com>
2018-02-20 16:45:24 -0600

    New Project
    add new project practice

---
+++ Homework/StatConPrac/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatConPrac
+{
+    public  class Bus
+    {
+        //static member (variable) used by all Bus instances 
+        protected static readonly DateTime globalStartTime;
+        
+        //property for the number of each Bus
+        protected int RouteNumber { get; set; }
+
+        //static constructor to initialize the static variable
+        //it is invoked before the first instance constructor is run (when the first object is created)
+        static Bus()
+        {
+            globalStartTime = DateTime.Now;
+            // The following statment produces the first line of output
+            //this line only occurs only once.
+            Console.WriteLine("The static constructor is sets the global start time to {0}",globalStartTime.ToLongTimeString());
+          
+        }
+
+        // declared an instance construct that takes one argument
+
+        public Bus(int routenum)
+        {
+            RouteNumber = routenum;
+            Console.WriteLine("Bus {0} is created ",RouteNumber);
+
+        }
+
+        //instance method
+        public void Drive()
+        {
+            TimeSpan elapsedTime = DateTime.Now - globalStartTime;
+            Console.WriteLine("{0} is starting it's Route {1:N2} mintues after global Start Time {2}.",this.RouteNumber,elapsedTime.TotalMilliseconds,globalStartTime.ToShortTimeString());
+        }
+
+
+        static void Main(string[] args)
+        {
+            // the creation of this object or instance activates the static constructor 
+            Bus bus1 = new Bus(72);
+
+
+            //creating a second bus. the static constructor will not be fired this time.
+            Bus bus2 = new Bus(117);
+
+            //send the bus on their way
+            bus1.Drive();
+            bus2.Drive();
+            Console.WriteLine("Press and key to Create.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Homework/TempCon/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempCon
+{
+    public static class TempCon
+    {
+        public static double CelToFah(string tempCel)
+        {
+            double cel = double.Parse(tempCel);
+            double fah = (cel * 9 / 5) + 32;
+            return fah;
+        }
+
+
+        public static double FahToCel(string tempFah)
+        {
+            double fah = double.Parse(tempFah);
+            double cel = (fah -32) * 5/9;
+            return cel;
+        }
+
+
+        static void Main(string[] args)
+        {
+            /*TempCon TC = new TempCon();*/ //public  double CelToFah(string tempCel) will make it work be removing static 
+
+            Console.WriteLine("Please Select Your option (Enter 1 or 2)");
+            Console.WriteLine("1.From Celsius to Fahrenheit.");
+            Console.WriteLine("2.From Fahrengeit to Celsius.");
+
+            string choice = Console.ReadLine();
+
+            double f, c = 0;
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Please Enter the celsius Temperature");
+                    f = CelToFah(Console.ReadLine());
+                    Console.WriteLine("The Temperture in Fahrenheit is {0:f2}",f);
+
+                    break;
+
+                case "2":
+                    Console.Write("Please Enter the Fahrenheit Temperature");
+                    c = FahToCel(Console.ReadLine());
+                    Console.WriteLine("The Temperture in Celsuis is {0:f2}", c);
+
+                    break;
+
+
+                    default:
+                    Console.WriteLine("Please select Your Choice: ");
+                    break;
+
+
+
+            }
+            Console.WriteLine("Please Enter any key to Quit");
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
